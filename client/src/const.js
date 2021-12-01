2310f62313c27cefd7d26aa2d83cbcb2ad84f413
knshika <poojakumari17032001@gmail.com>
2021-12-01 23:12:21 +0530

    🐛 Fix prod url

--- client/src/const.js
+++ client/src/const.js
@@ -1,4 +1,4 @@
 export const IS_DEV = import.meta.env.DEV
 export const API_URL = IS_DEV
   ? "http://localhost:8000"
-  : "https://event-management-web-app-production.up.railway.app/"
+  : "https://event-management-web-app-production.up.railway.app"
