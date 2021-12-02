68019aa9418957eca0176c1604587ee5a0b12578
knshika <poojakumari17032001@gmail.com>
2021-12-02 20:55:32 +0530

    🔥 enabled time stamps

--- server/model/User.js
+++ server/model/User.js
@@ -1,52 +1,55 @@
 const mongoose = require("mongoose")
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    max: 255,
-    min: 6,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      max: 255,
+      min: 6,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      max: 255,
+      min: 6,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      min: 8,
+    },
+    rollNo: {
+      type: Number,
+      unique: true,
+      required: true,
+      length: 7,
+    },
+    branch: {
+      type: String,
+      required: true,
+      max: 255,
+      min: 3,
+    },
+    batch: {
+      type: Number,
+      required: true,
+      length: 4,
+    },
+    mobileNo: {
+      type: Number,
+      unique: true,
+      required: true,
+      length: 10,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    max: 255,
-    min: 6,
-  },
-  dateOfBirth: {
-    type: Date,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    max: 1024,
-    min: 8,
-  },
-  rollNo: {
-    type: Number,
-    unique: true,
-    required: true,
-    length: 7,
-  },
-  branch: {
-    type: String,
-    required: true,
-    max: 255,
-    min: 3,
-  },
-  batch: {
-    type: Number,
-    required: true,
-    length: 4,
-  },
-  mobileNo: {
-    type: Number,
-    unique: true,
-    required: true,
-    length: 10,
-  },
-})
+  { timestamps: true }
+)
 
 module.exports = mongoose.model("User", userSchema)
