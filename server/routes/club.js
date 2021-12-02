87cc6c8685382d9ab9829f42972f1f86846466d3
knshika <poojakumari17032001@gmail.com>
2021-12-02 20:24:28 +0530

    ✨ create club api

---
+++ server/routes/club.js
@@ -0,0 +1,25 @@
+const router = require("express").Router()
+const Club = require("../model/Club")
+const User = require("../model/User")
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, admin_emails } = req.body
+    const admins = []
+
+    for (const email of admin_emails) {
+      const user = await User.findOne({ email }).exec()
+      admins.push(user._id)
+    }
+
+    const club = new Club({ name, admins })
+
+    await club.save()
+    res.send({ club })
+  } catch (err) {
+    console.log(err)
+    res.status(400).send(err)
+  }
+})
+
+module.exports = router
