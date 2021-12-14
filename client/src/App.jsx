3331b50bba5e030a784a08af218a828d17865b82
knshika <poojakumari17032001@gmail.com>
2021-12-14 09:03:06 +0530

    ⚡️ made create club page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
+import CreateClub from "./pages/CreateClub"
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="register" exact element={<Register />} />
           <Route path="login" exact element={<Login />} />
+          <Route path="club" exact element={<CreateClub />} />
           {/* <Route path="dashboard" exact element={<Dashboard />} /> */}
         </Routes>
       </BrowserRouter>
