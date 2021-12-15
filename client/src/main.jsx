a008d9b29dc4aa5052ae568e00b4546ce78dbd1d
knshika <poojakumari17032001@gmail.com>
2021-12-15 11:38:43 +0530

    ✨ created login State

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import "twind/shim"
 import { setup } from "twind"
+import { store } from "./state"
+import { Provider } from "react-redux"
 
-setup()
+setup({
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["'Readex Pro'", "sans-serif"],
+      },
+    },
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 )
