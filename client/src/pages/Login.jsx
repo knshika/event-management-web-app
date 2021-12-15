f1dd7d61b3491421b83b60eda8c4173484fd4677
knshika <poojakumari17032001@gmail.com>
2021-12-15 11:38:43 +0530

    ✨ created login State

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React from "react"
+import { useDispatch } from "react-redux"
 import { Forms } from "../components/Forms"
 import { API_URL } from "../const"
+import { login } from "../state/slices/loginSlice"
 
 const loginInputs = [
   { name: "email", type: "email", label: "Email Id" },
@@ -8,6 +10,8 @@ const loginInputs = [
 ]
 
 const Login = () => {
+  const dispatch = useDispatch()
+
   async function loginUser(formData) {
     const response = await fetch(`${API_URL}/api/user/login`, {
       method: "POST",
@@ -18,20 +22,19 @@ const Login = () => {
     })
     console.log(response)
 
-    const data = await response.json()
-
     if (response.status === 200) {
-      localStorage.setItem("token", data.token)
+      const data = await response.json()
+      console.log(data)
+      const { user, token } = data
+      localStorage.setItem("token", token)
       alert("Successfully logged in")
-      //   window.location.href = "/dashboard"
+      dispatch(login({ user, token }))
     } else {
       alert("Something is wrong!! Check your email or password")
     }
-
-    console.log(data.user)
   }
   return (
-    <div className="flex flex-col align-center items-center m-2">
+    <div className="flex flex-col justify-center items-center m-2">
       <h1 className="text-xl uppercase">Login </h1>
       <Forms inputs={loginInputs} onSubmit={loginUser} />
     </div>
