d322d8304683692bc0395510533e39ed9e7f129c
knshika <poojakumari17032001@gmail.com>
2021-12-16 19:15:52 +0530

    🚑 Made page for user when he login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import Register from "./pages/Register"
 import Login from "./pages/Login"
 import CreateClub from "./pages/CreateClub"
 import Navbar from "./components/Navbar"
+import UpdateClub from "./pages/UpdateClub"
+import ClubPage from "./pages/ClubPage"
+import Dashboard from "./pages/Dashboard"
 import { useDispatch } from "react-redux"
 import fetcher from "./utils/fetcher"
 import { login, logout } from "./state"
@@ -35,6 +38,10 @@ const App = () => {
         <Route path="register" exact element={<Register />} />
         <Route path="login" exact element={<Login />} />
         <Route path="club" exact element={<CreateClub />} />
+        <Route path="club/:id/update" exact element={<UpdateClub />} />
+        <Route path="club/:id" exact element={<ClubPage />} />
+        <Route path="user/:id" exact element={<Dashboard />} />
+
         {/* <Route path="dashboard" exact element={<Dashboard />} /> */}
       </Routes>
     </BrowserRouter>
