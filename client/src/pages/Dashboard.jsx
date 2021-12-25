2d01b40e9fd2934a61c15f6145a67d4f08111956
knshika <poojakumari17032001@gmail.com>
2021-12-26 00:19:09 +0530

    ⚡️ separated dashboards for admins and super admin

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react"
 import fetcher from "../utils/fetcher"
 import { useNavigate, useParams } from "react-router-dom"
+import { useLoginState } from "../state/slices/loginSlice"
 
 const Dashboard = () => {
   const { id } = useParams()
   const navigate = useNavigate()
+  const { user } = useLoginState()
   const [userDetails, setUserDetails] = useState(null)
 
   const getUserDetails = async () => {
@@ -34,6 +36,46 @@ const Dashboard = () => {
         >
           Update Profile
         </button>
+        {user?.superAdmin && (
+          <button
+            className=" m-2 py-2 px-1 border-2 rounded-lg uppercase bg-gray-300"
+            onClick={() => navigate("/club")}
+          >
+            Add Club
+          </button>
+        )}
+        {userDetails?.adminOfClub?.length > 0 && (
+          <div>
+            <h3>Your clubs</h3>
+            {userDetails.adminOfClub.map((club, index) => (
+              <div className="flex " key={index}>
+                <div>{club.name}</div>
+                <button
+                  className=" m-2 py-2 px-1 border-2 rounded-lg uppercase bg-blue-200"
+                  onClick={() => navigate(`/club/${club._id}`)}
+                >
+                  View Details
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
+        {userDetails?.participatedEvents?.length > 0 && (
+          <div>
+            <h3>Your registered Events</h3>
+            {userDetails.participatedEvents.map((event, index) => (
+              <div className="flex " key={index}>
+                <div>{event.name}</div>
+                <button
+                  className=" m-2 py-2 px-1 border-2 rounded-lg uppercase bg-blue-200"
+                  onClick={() => navigate(`/event/${event._id}`)}
+                >
+                  View Details
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     )
   )
